2491ea87e2271cb204d9fc0b6392777e3faa998d
Sindri Petur Laptop <sindripetur@gmail.com>
2017-09-06 21:04:11 +0000

    crawler socket

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const request = require('request');
-const cheerio = require('cheerio');
 
 const path = require('path');
 const crawl = require('./crawl');
@@ -23,13 +21,19 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 var messageHladinn = [];
+var clients = [];
 
 io.on('connection', function(socket){
+  clients.push(socket.id);
+  console.log(clients);
+
   console.log('a user connected');
   socket.emit('updateChat', messageHladinn);
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    var index = clients.indexOf(socket.id);
+    clients.splice(index,1);
   });
 
   socket.on('chat message', function(msg){
@@ -39,61 +43,15 @@ io.on('connection', function(socket){
     }
 
     io.emit('updateChat', messageHladinn)
+
+    crawl.crawler((data) => {
+      io.emit('updateSkraning', data)
+    })
   });
 });
 
 app.get('/', (req,res) => {
   //res.redirect('index2.html');
-});
-
-app.get('/scrape', (req, res) => {
-    url = 'https://nord.is/';
-
-    request(url, (error, response, html) => {
-        if(error) {
-          res.status(400).send();
-        }
-        var $ = cheerio.load(html);
-        const x = $('h2 a').attr('href');
-        if (x) {
-
-          request(url+x, (error, response, html) => {
-            if (error) {
-              res.status(400).send();
-            }
-              $ = cheerio.load(html);
-              var queue = [];
-
-              const y = $('.registered-members li');
-
-              var bidlisti = 0;
-              var mSkrad = false;
-
-              for(var i = 0; i < y.length; i++) {
-                console.log(i)
-                if (y[i].attribs.class) {
-                  bidlisti++;
-                }
-                if (y[i].children[0].data.indexOf('Melkorka Mjöll') !== -1) {
-                  mSkrad = true;
-                  break;
-                }
-              }
-
-              if (!mSkrad) {
-                return res.send({message: 'Gleymdi að skrá sig :('})
-              }
-              if (bidlisti === 0) {
-                return res.send({message: 'JÁ!', bidlisti});
-              } else {
-                return res.send({message: 'NEI', bidlisti})
-              }
-
-          });
-        } else {
-          return res.status(400).send();
-        }
-    });
 });
 
 http.listen(port, () => {
