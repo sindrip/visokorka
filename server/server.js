7f2a6c4e125b238421285e6dcdd66bd7137ea624
Sindri Petur Laptop <sindripetur@gmail.com>
2017-09-06 17:51:12 +0000

    init

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+// PARSE REQ
+app.use(bodyParser.json());
+
+// LOGGING
+app.use(morgan('dev'));
+
+// PUBLIC SERVE
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+  res.redirect('/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+})
